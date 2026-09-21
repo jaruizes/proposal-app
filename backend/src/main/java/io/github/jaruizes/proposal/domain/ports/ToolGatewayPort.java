49f2677eab0318491025ffe509f0a68ab65b142d
José Alberto Ruiz Casarrubios <jalb80@gmail.com>
2026-09-22 01:58:20 +0200

    Remove Spring-owned MCP integration

--- backend/src/main/java/io/github/jaruizes/proposal/domain/ports/ToolGatewayPort.java
+++
@@ -1,3 +0,0 @@
-package io.github.jaruizes.proposal.domain.ports;
-import java.util.Map;
-public interface ToolGatewayPort { Map<String,Object> execute(String toolName, Map<String,Object> arguments); }
