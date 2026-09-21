4eff70b1d1e52b3062a22da22793f329b450b480
José Alberto Ruiz Casarrubios <jalb80@gmail.com>
2026-09-22 01:06:48 +0200

    Export standard agent execution envelopes

--- agent-platform/src/agent_platform/domain/__init__.py
+++ agent-platform/src/agent_platform/domain/__init__.py
@@ -2,7 +2,7 @@
 
 from agent_platform.domain.cognitive import CognitiveContext, CognitiveContextItem, CognitiveSection, EpistemicLabel
 from agent_platform.domain.definitions import AgentConstraints, AgentDefinition, ModelPolicy, SkillDefinition
-from agent_platform.domain.executions import AgentArtifact, AgentError, AgentExecution, AgentExecutionRequest, AgentExecutionResult, AgentUsage, Attachment, ExecutionStatus
+from agent_platform.domain.executions import AgentArtifact, AgentError, AgentExecution, AgentExecutionCommandEnvelope, AgentExecutionEventEnvelope, AgentExecutionRequest, AgentExecutionResult, AgentUsage, Attachment, ExecutionStatus
 from agent_platform.domain.knowledge import DEFAULT_KNOWLEDGE_BASES, KnowledgeBase, KnowledgeChunk, KnowledgeDocument, KnowledgeDocumentStatus
 from agent_platform.domain.memory import MemoryEntry, MemoryKind, MemoryRecall, MemoryScope, MemoryScopeType, MemoryWrite
 from agent_platform.domain.ontology import OntologyConcept, OntologyMapping, OntologyRelationship, OntologyTargetType
@@ -10,7 +10,7 @@ from agent_platform.domain.retrieval import RetrievalFilters, RetrievalHit, Retr
 from agent_platform.domain.tools import ToolCall, ToolDefinition, ToolError, ToolResult
 
 __all__ = [
-    "AgentArtifact","AgentConstraints","AgentDefinition","AgentError","AgentExecution","AgentExecutionRequest","AgentExecutionResult","AgentUsage","Attachment","ExecutionStatus","ModelPolicy","SkillDefinition",
+    "AgentArtifact","AgentConstraints","AgentDefinition","AgentError","AgentExecution","AgentExecutionCommandEnvelope","AgentExecutionEventEnvelope","AgentExecutionRequest","AgentExecutionResult","AgentUsage","Attachment","ExecutionStatus","ModelPolicy","SkillDefinition",
     "CognitiveContext","CognitiveContextItem","CognitiveSection","EpistemicLabel",
     "ToolCall","ToolDefinition","ToolError","ToolResult","KnowledgeBase","KnowledgeDocument","KnowledgeChunk","KnowledgeDocumentStatus","DEFAULT_KNOWLEDGE_BASES",
     "RetrievalFilters","RetrievalHit","RetrievalMode","RetrievalQuery","RetrievalResult",
